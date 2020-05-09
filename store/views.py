0e2b1b8ab5925de0a5a14e46426e7660d617fbf5
marktitchen <bliss.2011@yahoo.co.uk>
2020-05-09 13:04:42 +0100

    User authentication login form completed

--- store/views.py
+++ store/views.py
@@ -6,6 +6,8 @@ import stripe
 from django.conf import settings
 from django.contrib.auth.models import Group, User
 from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 
 def home(request, category_slug=None):
@@ -182,3 +184,23 @@ def signupView(request):
     else:
         form = SignUpForm()
     return render(request, 'sign-up.html', {'form': form})
+
+def signinView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                return redirect('signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'sign-in.html', {'form': form})
+
+def signoutView(request):
+    logout(request)
+    return redirect('signin')
